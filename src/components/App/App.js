5b3d5a345dbd845ab1d7452595668c32ceeee309
Student <example@example.com>
2019-07-22 15:29:46 -0700

    add images for markers and favicons

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import { GoogleMap, withScriptjs, withGoogleMap, Marker } from 'react-google-maps';
+import { GoogleMap, withScriptjs, withGoogleMap, Marker, InfoWindow } from 'react-google-maps';
 import * as parkData from '../../data/skateboard-parks.json';
 
 console.log(process.env.REACT_APP_GOOGLE_KEY);
 
 function Map() {
+  const [selectedPark, setSelectedPark] = useState(null);
+
   return (
     <GoogleMap 
       defaultZoom={10}
-      defaultCenter={{ lat: 45.4211, lng: -75.6903 }}
+      defaultCenter={{ lat: 45.4211, lng: -75.6903 }} 
     >
       {parkData.features.map(park => (
         <Marker
@@ -18,8 +20,31 @@ function Map() {
             lat: park.geometry.coordinates[1],
             lng: park.geometry.coordinates[0]
           }}
+          onClick={() => {
+            setSelectedPark(park);
+          }}
+          icon={{
+            url: '/logo.png',
+            scaledSize: new window.google.maps.Size(25, 25)
+          }}
         />
       ))}
+      {selectedPark && (
+        <InfoWindow
+        position={{
+          lat: selectedPark.geometry.coordinates[1],
+          lng: selectedPark.geometry.coordinates[0]
+        }}
+        onCloseClick={() => {
+          setSelectedPark(null);
+        }}
+        >
+          <div>
+            <h2>{selectedPark.properties.NAME}</h2>
+            <p>{selectedPark.properties.DESCRIPTIO}</p>
+          </div>
+        </InfoWindow>
+      )}
     </GoogleMap>
   );
 }
@@ -31,7 +56,6 @@ export default function App() {
     <div style={{width: '100vw', height: '100vh'}}>
       <WrappedMap 
         googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_KEY}`}
-        // googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyCGWF4mOvMy60qFocJFXQvw9_X7ARA1_hM`}
         loadingElement={<div style={{ height: `100%` }} />}
         containerElement={<div style={{ height: `100%` }} />}
         mapElement={<div style={{ height: `100%` }} />}
